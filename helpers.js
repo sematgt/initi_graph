2e25b614446f67a3786795c26995d3470cb9f2d5
sgbliznyuk <sgbliznyuk@gmail.com>
2021-03-22 20:42:02 +0300

    added deepClone helper

---
+++ helpers.js
@@ -0,0 +1,30 @@
+function deepClone(obj) {
+    var copy;
+
+    if (null == obj || "object" != typeof obj) return obj;
+
+    if (obj instanceof Date) {
+        copy = new Date();
+        copy.setTime(obj.getTime());
+        return copy;
+    }
+
+    if (obj instanceof Array) {
+        copy = [];
+        for (var i = 0, len = obj.length; i < len; i++) {
+            copy[i] = deepClone(obj[i]);
+        }
+        return copy;
+    }
+
+    if (obj instanceof Object) {
+        copy = {};
+        for (var attr in obj) {
+            if (obj.hasOwnProperty(attr)) copy[attr] = deepClone(obj[attr]);
+        }
+        return copy;
+    }
+
+}
+
+export default deepClone;
